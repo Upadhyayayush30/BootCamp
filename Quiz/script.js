bad1fc468025a63d7e31507eb73dbe0475ec83fe
Ayush Kumar Upadhyay <115970349+Upadhyayayush30@users.noreply.github.com>
2024-05-29 10:03:45 +0530

    Add files via upload
    Its a simple quiz developed using HTML CSS Js

---
+++ Quiz/script.js
@@ -0,0 +1,82 @@
+const quizData = [
+    {
+        question: "What is 5 + 3?",
+        a: "5",
+        b: "8",
+        c: "10",
+        d: "15",
+        correct: "b",
+    },
+    {
+        question: "What is 12 - 4?",
+        a: "6",
+        b: "8",
+        c: "9",
+        d: "10",
+        correct: "b",
+    },
+    {
+        question: "What is 3 * 4?",
+        a: "7",
+        b: "10",
+        c: "12",
+        d: "14",
+        correct: "c",
+    },
+];
+
+const quiz = document.getElementById('quiz');
+const answerEls = document.querySelectorAll('.answer');
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswers();
+    const currentQuizData = quizData[currentQuiz];
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+    let answer;
+    answerEls.forEach(answerEl => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false);
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected();
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+                <button onclick="location.reload()">Reload</button>
+            `;
+        }
+    }
+});
